55cb77032548936a81104363880e772fc15f31b0
Tomasz Morawiec <moraviec@gmail.com>
2019-09-04 22:54:27 +0200

    final

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ This is part of iHome Project. PAW AGH 2019
 
 First version of server for controlling arduino device via firmata(johnny-five) and socket.io.
 For security it use JWT (https is necessary)
+this is part of iHome project
+
+1. connect arduino with firmata (leds in PWM pins)
+2. start rails server (database)
+3. node index.js
--- index.js
+++ index.js
@@ -7,48 +7,40 @@ const axios = require('axios');
 const debounceQueue = require ('debounce-queue');
 
 const board = new five.Board({port: 'COM4'});
-const switchAPI = 'http://localhost:3000/switches.json';
-const historyAPI = 'http://localhost:3000/histories';
+const API = 'http://localhost:3000'
+const switchAPI = `${API}/switches.json`;
+const historyAPI = `${API}/histories`;
 
+const secret = 'd105bba7e66439af4bdf70653143aca9a69fc87db14c299ffd2457ac2ec52ac64758044a521f45d3eed469dfbdbb1b58f38c2f692bda99cb264acb06b80f0858'
 var ledy = [];
 
 
 filterAndSave = (objects) => {
   // unfiltred data
   const history = objects.map(i=>i[0])
-
   // filtred by max timeStamp in unique names
   const lastChanges = Object.values(history.reduce(
     (r, o) => { 
       r[o.name] = r[o.name] && r[o.name].timeStamp > o.timeStamp ? r[o.name] : o
       return r }, {}))
-
   // save to DB filtred data
   lastChanges.map(x => postStateToHistory(x));
-
   console.log(ledy)
 } 
 
-
-
 const debouncedSaveToDB = debounceQueue(filterAndSave, 3000);
- 
-
 
 const getStateFromServer = async () => {
   try {
     const state = await axios.get(switchAPI);   
     ledy = state.data
     console.log(ledy)
-
-
   } catch (error) {
     console.error(error);
   }
 }
 
 const postStateToHistory = async (obj) => {
-
   axios.post(historyAPI, {
     "switch_name": obj.name,
     "user_name": obj.user,
@@ -56,7 +48,7 @@ const postStateToHistory = async (obj) => {
     "timeStamp": obj.timeStamp,
   })
   .then(function (response) {
-    console.log(`Użytkownik ${response.data.user} zapisał w Bazie danych stan switcha: ${response.data.switch}, wartość: ${response.data.value}`);
+    console.log(`User ${response.data.user} saves actual state of: ${response.data.switch}, to database - state is: ${response.data.value}`);
   })
   .catch(function (error) {
     console.log(error);
@@ -64,37 +56,9 @@ const postStateToHistory = async (obj) => {
 }
 
 getStateFromServer()
-// postStateToHistory()
-
-
-/// DEBOUNCE SAVE STATE IN API///
-
-// const saveData = () => {
-//   // wysyłanie ostatniego stanu arduino do API rails - historia przełączników
-//   console.log("Zapisywanie danych ..");
-
-// }
-
-// const debounce = function (fn, d) {
-//   let timer;
-//   return function () {
-//     let context = this,
-//       args = arguments;
-//     clearTimeout(timer);
-//     timer = setTimeout(() => {
-//       saveData.apply(context, arguments);
-//     }, d);
-//   }
-// }
-
-// const save = debounce(saveData, 2000);
-
-/// DEBOUNCE SAVE STATE IN API ///
 
 
 board.on("ready", () => {
-
-  
     const leds = ledy.map(led => new five.Led(led.pin))
     const values = ledy.map(el => parseInt(el.state.value))
     const names = ledy.map(el => el.name)
@@ -127,16 +91,12 @@ board.on("ready", () => {
     }
 
 setLedStates(leds,values)
-  
- 
-  
 
-    
 
   // initialize socket.io with JWT
   io.use(function(socket, next){
     if (socket.handshake.query && socket.handshake.query.token){
-      jwt.verify(socket.handshake.query.token, 'd105bba7e66439af4bdf70653143aca9a69fc87db14c299ffd2457ac2ec52ac64758044a521f45d3eed469dfbdbb1b58f38c2f692bda99cb264acb06b80f0858', function(err, decoded) {
+      jwt.verify(socket.handshake.query.token, secret, function(err, decoded) {
         if(err) return next(new Error('Authentication error'));
         socket.decoded = decoded;
         next();
@@ -148,21 +108,10 @@ setLedStates(leds,values)
   .on('connection', function(socket) {
       console.log(socket.id);
       console.log(socket.decoded);
-      // Connection now authenticated to receive further events
 
-      // wysłanie do nowo podłączonego użytkownika tablicy z aktualnym stanem ledów
+      // sending actual state of leds to new connected client
       socket.emit('hello', ledy);
-  
-      // socket.on('SEND_MESSAGE', function(data){
-      //   console.log(data);
-      //   // TU BĘDĄ WSTAWIONE DANE KTO WYWOŁAŁ TĄ AKCJĘ:
-      //   socket.broadcast.emit ('FromAPI', `wiadomość zwrotna wysłana przez ${socket.id}`);
-      // })
-  
-
-
-
-
+      // accept data from client and sets arduino and so on..
       socket.on('switch', function(data){
         let pinName = data[0]
         let value = parseInt(data[1])
@@ -171,76 +120,29 @@ setLedStates(leds,values)
         let ledPosition = names.indexOf(pinName)
 
         checkPermission = (name) => userPermission[name];
-
         if (checkPermission(pinName)) {
-          console.log(`${Date.now()} Użytkownik ${userName} ${socket.id} zmienia stan pinu ${pinName} na: ${value}`)
-
-          leds[ledPosition].brightness(value);
-
-          // ustawienie tablicy led aktulną wartością
-          ledy[ledPosition].state.value = value
+          console.log(`${Date.now()} User ${userName} ${socket.id} sets ${pinName} to: ${value}`)
           
-   
-            // jako obiekt w tablicy
-            debouncedSaveToDB({
-              name: pinName,
-              value: value,
-              user: userName,
-              timeStamp: Date.now()
-            })
-
-          // emituje dane do innych klientów oprócz samego wysyłającego 
+          // set arduino
+          leds[ledPosition].brightness(value);
+          // update led array
+          ledy[ledPosition].state.value = value
+          // save unique data do remote DB
+          debouncedSaveToDB({
+            name: pinName,
+            value: value,
+            user: userName,
+            timeStamp: Date.now()
+          })
+          // emit data to another connected clients with actual state 
           socket.broadcast.emit ('update-switch', data);
           
-        } else { console.log('Użytkownik nie ma praw do zmiany stanu tego pinu lub pin nie istnieje') }
-
-        })
-
-      // socket.on('slider', function(data){
-      //   // loguje dane
-      //   console.log(`Lampke zapalił ${socket.id} i ustawił ${data}`);
-      //   // if (socket.decoded.permission.reduce(data) == ) {
-          
-      //   // } else {
-          
-      //   // }
-        
-      //   // data.switchName
-      //   // data.value
-
-        
-
-      //   // leds[1].brightness(data);
-   
-      //   // save();
-      //   // // lightControl(data)
-        
-    
-      //   // // emituje dane do innych klientów oprócz samego wysyłającego 
-      //   // socket.broadcast.emit ('dane_zmiana_suwaka', data);
-      //   })
-  
+        } else { 
+          console.log('You dont have permission or this pin is not avalible') 
+        }
+      })
   });
-
- 
-
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
  
 http.listen(3030, function(){
